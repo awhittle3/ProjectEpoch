2f2bbabde86fa0883bd612ce0b18317d7d94b34a
Andrew Whittle <awhittle@ualberta.ca>
2017-08-16 15:27:12 -0600

    Initial code for charging bomb
    Player is no longer a ragdoll
    Arm rotation implemented

---
+++ ProjectEpoch/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public Transform arms;
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			
+			arms.Rotate (Vector3.right, rotationSpeed * Time.deltaTime);
+
+			chargeBomb ();
+		} else {
+
+		}
+	}
+
+	void chargeBomb(){
+
+	}
+}
